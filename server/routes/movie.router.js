28a812f43cfb869320fd767add8e214f179684b6
Rider <goejrider@gmail.com>
2022-11-04 10:10:26 -0500

    Add get target movie router

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -16,6 +16,24 @@ router.get('/', (req, res) => {
 
 });
 
+router.get('/:id', (req, res) => {
+  const sqlText = `SELECT "genres"."name", * FROM "movies_genres" 
+  JOIN "movies" ON "movies"."id" = "movie_id" 
+  JOIN "genres" ON "genres"."id" = "genre_id"
+  WHERE "movie_id" = $1;`
+  const sqlParams = [req.params.id]
+  pool.query(sqlText,sqlParams)
+    .then(dbRes => {
+      console.log('got movie!', dbRes.rows);
+      res.send(dbRes.rows);
+      res.sendStatus(200);
+    })
+    .catch(dbErr => {
+      console.error('error in getting movie', dbErr);
+      res.sendStatus(500)
+    })
+})
+
 router.post('/', (req, res) => {
   console.log(req.body);
   // RETURNING "id" will give us back the id of the created movie
