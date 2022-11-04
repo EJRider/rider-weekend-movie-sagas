b1e0e6f1f53140e76f76691baca992ef11e085a9
Rider <goejrider@gmail.com>
2022-11-04 09:47:35 -0500

    Add useHistory, useParams, useDispatch, useSelector to MovieDetails

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,6 +1,16 @@
-import {useHistory} from 
+import {useHistory} from 'react-router-dom';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
 function MovieDetails() {
+
+    const params=useParams();
+
+    useEffect(()=>{
+        console.log('loading detail page')
+    }, [params.id])
+
     return (
         <>
             <h1> Movie Details</h1>
